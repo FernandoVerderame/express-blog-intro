222d154b5dc72dcbf1cbba0c2a6fa9122395fd8e
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-24 14:48:53 +0200

    added port and host

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const express = require('express');
 
 // Inizializzo express
 const app = express();
+
+// Definisco le variabili port e host
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
