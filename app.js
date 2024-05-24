8ba8fd587ec01fbfbd4965d8647e41ccbffe9f7b
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-05-24 14:50:08 +0200

    added base route

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ const app = express();
 // Definisco le variabili port e host
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
+
+// Rotta base
+app.get('/', (req, res) => {
+    res.end(`<h1>Benvenuto nel mio blog!</h1>`);
+});
